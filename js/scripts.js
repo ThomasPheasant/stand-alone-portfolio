bb83e7db2f6a3ba239901a3909739f69c75decb4
ThomasPheasant <pheasantthomas@gmail.com>
2017-11-29 15:28:35 -0800

    Added images/fabrics and JS Random image generator

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 // Randomize Header
 
+// Store Images
+var fabric0 = url("../img/fabric.png");
+var fabric1 = url("../img/fabric1.png");
+var fabric2 = url("../img/fabric2.png");
+
 // Store & Generate Number
 var maxRand = 3;
 var numRand = Math.floor( Math.random() * maxRand );
-//console.log(numRand);
-
-// Store images
-var img0 = 'http://picsum.photos/200x200';
-var img1 = 'http://picsum.photos/300x200';
-var img2 = 'http://picsum.photos/400x200';
+console.log(numRand);
 
 // Show Images
-$('header img').attr('src', eval('img' + numRand) );
+$('fabric').attr( 'src', eval('fabric' + numRand));
+
+
+
+
+
+
+
+
+
 
 // Activate Gallery
 $('figure').lightGallery({
