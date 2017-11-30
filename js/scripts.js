1808797578b91867913bfc63a2ad32eb6e0cfa65
ThomasPheasant <pheasantthomas@gmail.com>
2017-11-30 14:15:53 -0800

    Version Control

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,19 @@
 // Randomize Header
 
 // Store Images
-var fabric0 = url("../img/fabric.png");
-var fabric1 = url("../img/fabric1.png");
-var fabric2 = url("../img/fabric2.png");
+//var fabric0 = url("../img/fabric.png");
+//var fabric1 = url("../img/fabric1.png");
+//var fabric2 = url("../img/fabric2.png");
 
 // Store & Generate Number
-var maxRand = 3;
-var numRand = Math.floor( Math.random() * maxRand );
-console.log(numRand);
+//var maxRand = 3;
+//var numRand = Math.floor( Math.random() * maxRand );
+//console.log(numRand);
 
 // Show Images
-$('body').addClass('bg' + numRand);
+//$('img').attr( 'src', eval('fabric' + numRand));
+
+//$('body').addClass('bg' + numRand);
 
 
 
