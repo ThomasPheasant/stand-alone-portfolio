18faa8f9b5bc696aaa2307f973177b8980b2e7f8
ThomasPheasant <tpheasant@vcd038805md.eastern.ewu.edu>
2017-12-07 11:18:55 -0800

    Random Header p generator

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 // Randomize Header
+//Store Headers
+var header0 = 'This is the portfolio portion of the website.';
+var header1 = 'All the images below are projects Ive done showing my ability and creativity.';
+var header2 = 'All images below were made by myself!';
+
 // Store & Generate Number
 var maxRand = 3;
 var numRand = Math.floor( Math.random() * maxRand );
@@ -6,8 +11,8 @@ console.log(numRand);
 
 // Show Images
 $('header').addClass('bg' + numRand);
-
-
+// Show Headers
+$('header p').text(eval("header" + numRand));
 
 
 // Activate Gallery
